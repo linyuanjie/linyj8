a0ed0d7eb34fed0ca7c12e65b8a6228d9747f06f
linyuanjie <296005189@qq.com>
2019-10-12 23:59:49 +0800

    Site updated: 2019-10-12 23:59:49

--- photos/ins.js
+++ photos/ins.js
@@ -117,7 +117,7 @@
           var data = res.list[j].arr;
           var liTmpl = "";
           for (var i = 0, len = data.link.length; i < len; i++) {
-            var minSrc = 'https://raw.githubusercontent.com/linyuanjie/blogPicture/master/min_photos/' + data.link[i] + '.min.jpg';
+            var minSrc = 'https://raw.githubusercontent.com/linyuanjie/blogPicture/master/min_photos/' + data.link[i];
             var src = 'https://raw.githubusercontent.com/linyuanjie/blogPicture/master/photos/' + data.link[i];
             var type = data.type[i];
             var target = src + (type === 'video' ? '.mp4' : '.jpg');
@@ -125,7 +125,7 @@
 
             liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">\
                 <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/photos/ins.j/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="https://raw.githubusercontent.com/linyuanjie/blogPicture/master/min_photos/2019-9-20_%E6%B2%B3%E6%B5%81.jpg" itemprop="thumbnail" onload="lzld(this)">\
                 </a>\
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
             </figure>';
